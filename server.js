e1ede91ab3b26297f9061cd0c509a72a410b8bc0
AlexandraBautistaChattin <balexandra96@gmail.com>
2022-09-15 18:37:23 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require ('express-react-views').createEngine())
 
+// MIDDLEWARE
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 }) 
 
 const breadsController = require('./controllers/breads_controllers.js') 
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 app.listen(PORT, () => {
     console.log('listen on port', PORT);
